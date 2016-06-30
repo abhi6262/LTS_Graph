1e5da8a9b7b8699e5b476cafc30bcc9200c94a1a
Debjit Pal <dpal2@illinois.edu>
2016-06-30 02:30:15 -0500

    Adding SIU to L2 monitor. Compilation checked for all four Inbound Monitors

--- monitors/inbound/siu_to_l2_mon.v
+++ monitors/inbound/siu_to_l2_mon.v
@@ -48,54 +48,172 @@ wire l2t5_sii_iq_dequeue = `SII.l2t5_sii_iq_dequeue;
 wire l2t6_sii_iq_dequeue = `SII.l2t6_sii_iq_dequeue;
 wire l2t7_sii_iq_dequeue = `SII.l2t7_sii_iq_dequeue;
 
+/* For L2 Tag 0 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t0_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 0, sii_l2t0_req_vld = %b", sii_l2t0_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 0");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t0_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t0_sii_iq_dequeue))
+begin
+    if(l2t0_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 1 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t1_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 1, sii_l2t1_req_vld = %b", sii_l2t1_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 1");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t1_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t1_sii_iq_dequeue))
+begin
+    if(l2t1_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 2 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t2_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 2, sii_l2t2_req_vld = %b", sii_l2t2_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 2");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t2_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t2_sii_iq_dequeue))
+begin
+    if(l2t2_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 3 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t3_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 3, sii_l2t3_req_vld = %b", sii_l2t3_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 3");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t3_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t3_sii_iq_dequeue))
+begin
+    if(l2t3_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 4 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t4_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 4, sii_l2t4_req_vld = %b", sii_l2t4_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 4");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t4_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t4_sii_iq_dequeue))
+begin
+    if(l2t4_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 5 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t5_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 5, sii_l2t5_req_vld = %b", sii_l2t5_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 5");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t5_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t5_sii_iq_dequeue))
+begin
+    if(l2t5_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 6 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t6_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 6, sii_l2t6_req_vld = %b", sii_l2t6_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 6");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t6_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
+
+always @(posedge (iol2clk && enabled && l2t6_sii_iq_dequeue))
+begin
+    if(l2t6_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
+
+/* For L2 Tag 7 */
 
 always @(posedge (iol2clk && enabled))
 begin
     if(sii_l2t7_req_vld)
-        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD T = 7, sii_l2t7_req_vld = %b", sii_l2t7_req_vld);
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "SII_L2T_REQ_VLD Tag = 7");
+        /* To get the Header cycle */
+        repeat(2) @(posedge iol2clk)
+        begin
+            `PR_INFO("siu_to_l2_mon", `INFO, "SII to L2T Header Cycle Header = %x", sii_l2t7_req);
+        end
+        /* To bypass the dummy cycle */
+        repeat(3) @(posedge iol2clk);
 end
 
+always @(posedge (iol2clk && enabled && l2t7_sii_iq_dequeue))
+begin
+    if(l2t7_sii_iq_dequeue)
+        `PR_ALWAYS("siu_to_l2_mon", `ALWAYS, "Read Requests from SIU Dequeued from L2Tag Input Queue");
+end
 
 endmodule
