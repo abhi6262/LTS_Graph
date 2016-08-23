61c4b6410b4d5585f86a16ce427039bb6d91ce59
Debjit Pal <dpal2@illinois.edu>
2016-08-23 18:28:22 -0500

    Checked Two Subroutines freq_Check and getEvents

--- episode_mining.py
+++ episode_mining.py
@@ -6,18 +6,22 @@ import numpy as np
 parser = agp.ArgumentParser(
         description='Episode Mining Code for Post-Silicon Traceability of Message Transactions\nAuthor: Debjit Pal\nEmail: dpal2@illinois.edu', formatter_class=agp.RawTextHelpFormatter
         )
-parser.add_argument("-w", "--window-length", help="Length of the window in cycles", required=True)
-parser.add_argument("-s", "--min-support", help="Minimum support value", required=True)
+parser.add_argument("-w", "--window-length", help="Length of the window in cycles", type=int, dest="window_length", required=True)
+parser.add_argument("-s", "--min-support", help="Minimum support value", type=int, dest="min_sup", required=True)
 #The following two parameters to segregate the message trace to make sure we are not mining from traces which are not relevant#
-parser.add_argument("-m", "--start-cycle", help="Start cycle of the trace", required=True)
-parser.add_argument("-M", "--stop-cycle", help="End cycle of the trace", required=True)
+parser.add_argument("-m", "--start-cycle", help="Start cycle of the trace",  type=int, dest="start_cycle",required=True)
+parser.add_argument("-M", "--stop-cycle", help="End cycle of the trace", type=int, dest="stop_cycle", required=True)
+parser.add_argument("-e", "--event-all", help="File containing all possible events", dest="event_all", required=True)
+parser.add_argument("-E", "--event-seq", help="File containing Events happened during execution", dest="event_seq", required=True)
 args = parser.parse_args()
 
 # Variables holding the runtime options
-window_length = args.window-length
-min_sup = args.min-support
-start_cycle = args.start-cycle
+window_length = args.window_length
+min_sup = args.min_sup
+start_cycle = args.start_cycle
 stop_cycle = args.stop_cycle
+event_all = args.event_all
+event_seq = args.event_seq
 
 class EpisodeMining():
     def __init__(
@@ -26,22 +30,44 @@ class EpisodeMining():
             ):
         self.enabled = enabled
 
+    def GetEvents(self, Event_Seq, nxtStart, pointer):
+        eventTuple = ()
+        currPoint = nxtStart
+        while True:
+            #print currPoint
+            if pointer >= len(Event_Seq):
+                break
+            currEventElements = Event_Seq[pointer].split('@')
+            #print currEventElements
+            currPoint = int(currEventElements[1])
+            if currPoint > nxtStart + window_length:
+                break
+            eventTuple = eventTuple + tuple([currEventElements[0].lstrip().rstrip()])
+            pointer = pointer + 1
+        #print eventTuple
+        return eventTuple, pointer
+        
     def Freq_Check(self, Event_Set, Event_Seq):
+        '''
+        Event Set: A Tuple
+        Event Req: A Tuple
+        '''
         FreqEpisode = {}
-        pointer = start_cycle
-        nxtStart = 0
-        while pointer <= stop_cycle:
-            eventTuple, nxtStart = self.GetEvents(Event_Set, nxtStart)
+        pointer = 0
+        nxtStart = start_cycle
+        while nxtStart <= stop_cycle:
+            eventTuple, pointer = self.GetEvents(Event_Seq, nxtStart, pointer)
             for event in Event_Set:
-                if set(event) < set(eventTuple):
+                if set(event) <= set(eventTuple):
                     if event in FreqEpisode.keys():
                         FreqEpisode[event] = FreqEpisode[event] + 1
                     else:
                         FreqEpisode[event] = 1
-            pointer = pointer + window_length
+            nxtStart = nxtStart + window_length
         # After all the Frequencies of the Event Episode are calculated make
         # another pass to see which Episode is more frequent using the min_sup 
         # value. Keep those Event Sequence which is frequent enough
+        #print FreqEpisode
         for key in FreqEpisode.keys():
             if FreqEpisode[key] < min_sup:
                 try:
@@ -117,9 +143,42 @@ class EpisodeMining():
         return FreqEpisode
 
 
+class ReadData():
+    def __init__(
+            self,
+            enabled = 1,
+            ):
+        self.enabled = enabled
 
+    def ReadEventSet(self, EventFileName):
+        Event_Set = ()
+        with open(EventFileName) as f:
+            for line in f:
+                #print line.rstrip().lstrip()
+                Event_Set = Event_Set + tuple([line.rstrip().lstrip()])
+        f.close()
+        return Event_Set
+
+    def ReadEventSequence(self, EventSeqFile):
+        Event_Seq = ()
+        with open(EventSeqFile) as f:
+            for line in f:
+                #print line.rstrip().lstrip()
+                Event_Seq = Event_Seq + tuple([line.rstrip().lstrip()])
+        f.close()
+        return Event_Seq
 
 
 if __name__ == "__main__":
+
+    ReadData = ReadData()
+    EpisodeMining = EpisodeMining()
+
     print "Initializing Episode Mining with the following parameters:"
     print "Window length = " + str(window_length) + " Minimum Support = " + str(min_sup) + " Start Cycle = " + str(start_cycle) + " Stop Cycle = " + str(stop_cycle)
+    Event_Set = ReadData.ReadEventSet(event_all)
+    Event_Seq = ReadData.ReadEventSequence(event_seq)
+
+    FreqEpisode = EpisodeMining.Freq_Check(Event_Set, Event_Seq)
+    print FreqEpisode
+
