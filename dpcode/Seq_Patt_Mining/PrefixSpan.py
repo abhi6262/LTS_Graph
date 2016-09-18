8fc3a8a523e19d7e8d6cff3f04caab6b3382ee41
Debjit Pal <dpal2@illinois.edu>
2016-09-17 21:41:11 -0500

    Adding PrefixSpan Code along with example from the paper

--- dpcode/Seq_Patt_Mining/PrefixSpan.py
+++ dpcode/Seq_Patt_Mining/PrefixSpan.py
@@ -28,9 +28,50 @@ class ReadData():
         with open(EventSeqFile) as f:
             for line in f:
                 elements = line.lstrip().rstrip().split(',')
-                SDB[SID] = elements
+                itemSet = []
+                for e in elements:
+                    itemSet.append(e.split())
+                SDB[SID] = itemSet
                 SID = SID + 1
         return SDB
+
+    def PrintPatternInStringFormat(self, Pattern):
+        '''
+        Take Patterns in List of Lists format and convert them to a String Format for easy understanding
+        '''
+        stringToPrint = '<'
+        for element in Pattern:
+            if len(element) == 1:
+                stringToPrint = stringToPrint + element[0]
+            else:
+                stringToPrint = stringToPrint + '('
+                for item in element:
+                    stringToPrint = stringToPrint + item
+                stringToPrint = stringToPrint + ')'
+        stringToPrint = stringToPrint + '>'
+        return stringToPrint
+
+class SeqPattern():
+    def __init__(
+            self,
+            Sequence,
+            Support
+            ):
+        self.Sequence = []
+        for element in Sequence:
+            self.Sequence.append(list(element))
+        self.Support = Support
+
+    def Append(self, Pattern):
+        if Pattern.Sequence[0][0] == UNDERSCORE:
+            FirstElement = Pattern.Sequence[0]
+            FirstElement.remove(UNDERSCORE)
+            # Need to understand this
+            self.Sequence[-1].extend(FirstElement)
+            self.Sequence.extend(Pattern.Sequence[1:])
+        else:
+            self.Sequence.extend(Pattern.Sequence)
+        self.Support = min(self.Support, Pattern.Support)
 
 class PrefixSpan():
     def __init__(
@@ -39,56 +80,138 @@ class PrefixSpan():
             ):
         self.enabled = enabled
 
-    def FindSeqPatterns(self, SDB):
-        assert type(SDB) is DictType, "FindSeqPatterns: Expected \"SDB\" Dict Type. Received %r" % type(SDB) 
+    def FindFreqItems(self, Pattern, SDB, min_sup):
+        assert type(SDB) is ListType, "FindSeqPatterns: Expected \"SDB\" List Type. Received %r" % type(SDB) 
         try:
             assert(len(SDB))
         except AssertionError:
             print "SDB is empty"
-        # SeqPatterns is a Dictionary storing "local frquent patterns" along with its support
-        SeqPatterns = {}
+            return []
+        # FreqItems is a List storing "local frquent patterns"
+        FreqItems = []
+        Items = {}
+        Items_ = {}
+        # Stores the LastItem of the LastElement in the Pattern Sequence
+        LastElement = []
 
-        for key_ in SDB.keys():
-            # Consider one sequence
-            Sequence = SDB[key_]
-            # print Sequence
-            # Consider one transaction in a sequence
+        try:
+            assert(len(Pattern.Sequence))
+            LastElement = Pattern.Sequence[-1]
+        except AssertionError:
+            LastElement = []
 
-            # Finding Length 1 Sequence
+        for Sequence in SDB:
+            
+            IsPrefix = True
+            for item in LastElement:
+                if item not in Sequence[0]:
+                    IsPrefix = False
+                    break
+            if IsPrefix and len(LastElement) > 0:
+                Index = Sequence[0].index(LastElement[-1])
+                if Index < len(Sequence[0]) - 1:
+                    for item in Sequence[0][Index + 1:]:
+                        if item in Items_.keys():
+                            Items_[item] = Items_[item] + 1
+                        else:
+                            Items_[item] = 1
+            
+            if UNDERSCORE in Sequence[0]:
+                for item in Sequence[0][1:]:
+                    if item in Items_.keys():
+                        Items_[item] = Items_[item] + 1
+                    else:
+                        Items_[item] = 1
+                Sequence = Sequence[1:]
+
             UniqueItemSeenSoFarInThisSequence = []
             for element in Sequence:
                 # Consider every item in a transaction
                 for item in element:
-                    if item == ' ':
-                        continue
                     if item not in UniqueItemSeenSoFarInThisSequence:
                         UniqueItemSeenSoFarInThisSequence.append(item)
-                        if item not in SeqPatterns.keys():
-                            SeqPatterns[item] = 1
+                        if item in Items.keys():
+                            Items[item] = Items[item] + 1
                         else:
-                            SeqPatterns[item] = SeqPatterns[item] + 1
+                            Items[item] = 1
+       
+        FreqItems.extend([SeqPattern([[UNDERSCORE, Pattern]], Support)
+                            for Pattern, Support in Items_.iteritems()
+                            if Support >= min_sup])
 
-        return SeqPatterns
+        FreqItems.extend([SeqPattern([[Pattern]], Support)
+                            for Pattern, Support in Items.iteritems()
+                            if Support >= min_sup])
 
-    def FindProjectedDataBase(self, SeqPatterns, SDB):
-        assert type(SDB) is DictType, "FindProjectedDataBase: Expected \"SDB\" Dict Type. Received %r" % type(SDB)
-        assert type(SeqPatterns) is ListType, "FindProjectedDataBase: Expected \"SeqPatterns\" List Type. Received %r" % type(SeqPatterns)
-        return True
+        
+        SortedList = sorted(FreqItems, key = lambda Pattern: Pattern.Support)
+
+        return SortedList
+
+    def FindProjectedDataBase(self, Pattern, SDB):
+        assert type(SDB) is ListType, "FindProjectedDataBase: Expected \"SDB\" List Type. Received %r" % type(SDB)
+        assert type(Pattern.Sequence) is ListType, "FindProjectedDataBase: Expected \"Pattern\" List Type. Received %r" % type(Pattern)
+        # The following dictionary will contain the Sequences as the key and the projected database
+        # as the value of ket
+        #print "Received Database: ", pprint.pprint(SDB)
+        ProjectedDataBase = []
+        LastElementOfPattern = Pattern.Sequence[-1]
+        LastItemOfLastElementOfPattern = LastElementOfPattern[-1]
+        #print "LastElementOfPattern: ", LastElementOfPattern
+        #print "LastItemOfLastElementOfPattern: ", LastItemOfLastElementOfPattern
+        for Sequence in SDB:
+            #print "Sequence: ", Sequence
+            ProjectedSDB = []
+            for element in Sequence:
+                #print "Element: ", element
+                FoundPrefix = False
+                if UNDERSCORE in element:
+                    if LastItemOfLastElementOfPattern in element and len(LastElementOfPattern) > 1:
+                        FoundPrefix = True
+                else:
+                    FoundPrefix = True
+                    for item in LastElementOfPattern:
+                        if item not in element:
+                            FoundPrefix = False
+                            break
+
+                if FoundPrefix:
+                    ElementIndex = Sequence.index(element)
+                    #print ElementIndex
+                    LastItemOfLastElementOfPatternIndex = element.index(LastItemOfLastElementOfPattern)
+                    #print LastItemOfLastElementOfPatternIndex
+                    if LastItemOfLastElementOfPatternIndex == len(element) - 1:
+                        ProjectedSDB = Sequence[ElementIndex + 1:]
+                        #print "From If: ", ProjectedSDB
+                    else:
+                        ProjectedSDB = Sequence[ElementIndex:]
+                        elementModified = element[LastItemOfLastElementOfPatternIndex:]
+                        elementModified[0] = UNDERSCORE
+                        ProjectedSDB[0] = elementModified
+                        #print "From Else: ", ProjectedSDB
+                    break
+            if ProjectedSDB:
+                ProjectedDataBase.append(ProjectedSDB)
+
+        return ProjectedDataBase
 
 
-    def GeneratePrefix(self, SeqPatterns, LocalFreqItems):
-        assert type(SeqPatterns) is ListType, "GeneratePrefix: Expected \"SeqPatterns\" List Type. Received %r" % type(SeqPatterns)
-        assert type(LocalFreqItems) is DictType, "GeneratePrefix: Expected \"LocalFreqItems\" Dict Type. Received %r" % type(LocalFreqItems)
-
-        return True
-
-
-    def PrefixSpan(self, SDB, SeqPatterns, min_sup):
-        assert type(SDB) is DictType, "PrefixSpan: Expected \"SDB\" Dict Type. Received %r" % type(SDB)
-        assert type(SeqPatterns) is ListType, "PrefixSpan: Expected \"SeqPatterns\" List Type. Received %r" % type(SeqPatterns)
+    def PrefixSpan(self, Pattern, SDB, min_sup):
+        AllPatterns = []
+        assert type(SDB) is ListType, "PrefixSpan: Expected \"SDB\" List Type. Received %r" % type(SDB)
         assert type(min_sup) is IntType, "PrefixSpan: Expected \"min_sup\" Int Type. Received %r" % type(min_sup)
+        
+        FreqItems = self.FindFreqItems(Pattern, SDB, min_sup)
+        for Item_ in FreqItems:
+            SeqPattern_ = SeqPattern(Pattern.Sequence, Pattern.Support)
+            SeqPattern_.Append(Item_)
+            AllPatterns.append(SeqPattern_)
 
-
+            ProjectedDataBase = self.FindProjectedDataBase(SeqPattern_, SDB)
+            NewPatterns = self.PrefixSpan(SeqPattern_, ProjectedDataBase, min_sup)
+            AllPatterns.extend(NewPatterns)
+        
+        return AllPatterns
 
 
 if __name__ == "__main__":
@@ -101,8 +224,9 @@ if __name__ == "__main__":
     # the sequence. Every element/transaction/itemset will have one or more than one
     # item
     SDB = ReadData.ReadEventSequence('SDB_Data.dat')
-    #pprint.pprint(SDB)
-    SeqPatterns = PrefixSpan.FindSeqPatterns(SDB)
-    pprint.pprint(SeqPatterns)
-
-
+    SDB_ = []
+    for key in SDB.keys():
+        SDB_.append(SDB[key])
+    AllPatterns = PrefixSpan.PrefixSpan(SeqPattern([], sys.maxint), SDB_, 3)
+    for Pattern_ in AllPatterns:
+        print ReadData.PrintPatternInStringFormat(Pattern_.Sequence) + ' :: Support : ' + str(Pattern_.Support) 
